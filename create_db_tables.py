dde2a1b66075ac5b9ce4d3953fa499846b5d4d3b
afitzer <alexpfitzgerald@yahoo.com>
2022-09-25 16:41:07 -0500

    created the db tables with the newly created funcs

---
+++ create_db_tables.py
@@ -0,0 +1,10 @@
+import sqlite3
+import model
+
+model.create_offensive_teams_passing()
+
+model.create_offensive_teams_receiving()
+
+model.create_offensive_teams_rushing()
+
+model.create_offensive_teams_scoring()
