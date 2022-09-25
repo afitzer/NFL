fb5741539411149ade5ef5042efcc7bf3fb97312
afitzer <alexpfitzgerald@yahoo.com>
2022-09-25 18:37:00 -0500

    added some print statements

--- teams.py
+++ teams.py
@@ -24,6 +24,8 @@ soup_receiving = BeautifulSoup(r_receiving.text, 'html.parser')
 soup_rushing = BeautifulSoup(r_rushing.text, 'html.parser')
 soup_scoring = BeautifulSoup(r_scoring.text, 'html.parser')
 
+print("The HTML from the NFL Website done being parsed")
+
 # Get the table
 passing_table_in_html = soup_passing.find('table')
 receiving_table_in_html = soup_receiving.find('table')
@@ -40,6 +42,8 @@ passing_table_body = model.get_table_body_as_lists(passing_table_in_html)
 receiving_table_body = model.get_table_body_as_lists(receiving_table_in_html)
 rushing_table_body = model.get_table_body_as_lists(rushing_table_in_html)
 scoring_table_body = model.get_table_body_as_lists(scoring_table_in_html)
+
+print("The data is extracted from the HTML now, next up is the joining of the header and body!")
 
 # Join the table head data and table body data
 passing_table_data = [passing_table_head] + passing_table_body
@@ -69,6 +73,8 @@ passing_df = pd.DataFrame(data=passing_data)
 receiving_df = pd.DataFrame(data=receiving_data)
 rushing_df = pd.DataFrame(data=rushing_data)
 scoring_df = pd.DataFrame(data=scoring_data)
+
+print("The data is now in a Pandas DataFrame, next up is the cleaning of the data!")
 
 # Make the first row the column names
 passing_columns = passing_df.iloc[0]
@@ -102,6 +108,8 @@ receiving_df['Date'] = d1
 rushing_df['Date'] = d1
 scoring_df['Date'] = d1
 
+print("The data is now cleaned up, next up is the writing of the data to the database!")
+
 # Append new data to the sqlite3 database
 conn = sqlite3.connect('nfl.db')
 passing_df.to_sql('passing', conn, if_exists='append', index=False)
@@ -111,3 +119,5 @@ scoring_df.to_sql('scoring', conn, if_exists='append', index=False)
 
 # Close the connection
 conn.close()
+
+print("The program is complete!")
